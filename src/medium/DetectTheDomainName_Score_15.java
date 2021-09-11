25d69ac2038f5f433d0d3492c4e4f50c7d28fdcd
Hyago <hyagomvale@gmail.com>
2021-09-11 15:38:53 -0300

    Resolução da questão: Detect The Domain Name

---
+++ src/medium/DetectTheDomainName_Score_15.java
@@ -0,0 +1,21 @@
+package medium;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class DetectTheDomainName_Score_15 {
+
+	public static void main(String[] args) {
+		try(var scanner = new Scanner(new File("DetectTheDomainName_Score_15_TC2"))){
+			var set = new TreeSet<String>();
+			scanner.findAll("(?<=http[s]?://)(?:ww[w\\d]\\.)?([\\p{Alnum}-]+\\.(?:[\\p{Alnum}-]\\.?)+)")
+				.forEach(mResult -> set.add(mResult.group(1)));
+			System.out.println(set.toString().replaceAll("\\[|\\]|\\s", "").replaceAll(",", ";"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
