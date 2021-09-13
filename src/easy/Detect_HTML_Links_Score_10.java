f95ec58e164a9a101e6b65921c10c6ed130e06b2
Hyago <hyagomvale@gmail.com>
2021-09-12 21:53:55 -0300

    Melhorias

---
+++ src/easy/Detect_HTML_Links_Score_10.java
@@ -0,0 +1,16 @@
+package easy;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Detect_HTML_Links_Score_10 {
+	public static void main(String[] args) {
+		try (var scanner = new Scanner(new FileInputStream("Detect_HTML_Links_Score_10_TC1"))) {
+			scanner.findAll("<a\\s+.*?href=\\\"(.+?)\\\".*?>\s*(?:<.+?>)*(.*?)\s*</")
+					.forEach(mr -> System.out.printf("%s,%s%n", mr.group(1), mr.group(2)));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
